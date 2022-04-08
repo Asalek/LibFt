9af9e755d631f56f06245793cb28d30abbee71bc
Ayoub Salek <asalek8599@gmail.com>
2022-04-08 22:10:58 +0000

    add: count_word function
    this function count and return the number of word in a sentence

---
+++ LibFft_count_words.c
@@ -0,0 +1,18 @@
+size_t	count_words(char const *s, char c)
+{
+	size_t	count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		if (*s != c)
+		{
+			count++;
+			while (*s != '\0' && *s != c)
+				s++;
+		}
+		else
+			s++;
+	}
+	return (count);
+}
